189c31f0d1d7691aa92641ddf4fcd90f489919b6
said4ik <muxammadaminartikov@gmail.com>
2024-10-02 14:09:31 +0500

    D

---
+++ src/test/java/org/example/cloudproject/CloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
